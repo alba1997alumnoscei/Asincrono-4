685a6989f8adb654b8b8f4fee5785ad9c6812375
alba1997alumnoscei <alba.1997@alumnos.cei.es>
2024-07-02 20:41:03 +0200

    barritas de transicion

--- script.js
+++ script.js
@@ -1,74 +1,116 @@
 
-document.addEventListener('DOMContentLoaded', function() { 
-    
-    const btnSilla = document.querySelector('.footer-objeto_01');
-    const btnMesa = document.querySelector('.footer-objeto_02');
+// ponemos a funcionar al DOM
+document.addEventListener('DOMContentLoaded', function () {
 
+    //1. Declaramos variables 
+
+    // Agarramos elementos: 
+    //texto silla, imagen silla, texto mesa, imagen mesa 
     const txtSilla = document.querySelector('.txt-silla');
     const imgSilla = document.querySelector('.img-silla');
     const txtMesa = document.querySelector('.txt-mesa');
     const imgMesa = document.querySelector('.img-mesa');
+    //botones del footer 
+    const btnSilla = document.querySelector('.footer-objeto_01');
+    const btnMesa = document.querySelector('.footer-objeto_02');
 
-    
+    //barritas de transición 
+    const barritaSilla = document.querySelector('.barrita-silla');
+    const barritaMesa = document.querySelector('.barrita-mesa');
 
 
+    //Cuando cargue la página que se vea solo la silla (img y txt) 
     txtSilla.style.display = 'flex';
     imgSilla.style.display = 'block';
     txtMesa.style.display = 'none';
     imgMesa.style.display = 'none';
+    barritaSilla.style.display = 'block';
+    
     
 
-    btnSilla.addEventListener('click', function() {
-        // Mostrar texto e imagen de la silla
-        document.querySelector('.txt-silla').style.display = 'flex';
-        document.querySelector('.img-silla').style.display = 'block';
-        document.querySelector('.barrita-silla').style.display = 'block';
+    // 2 . Funciones 
+
+    //Cuando hago click sobre el botón de la silla
+    btnSilla.addEventListener('click', function () {
+
+        // Se mostrarán 
+        txtSilla.style.display = 'flex';
+        imgSilla.style.display = 'block';
+        barritaSilla.style.display = 'block';
+        barritaSilla.classList.toggle('completa');
+        btnSilla.classList.toggle('completa');
 
         // Ocultar texto e imagen de la mesa
-        document.querySelector('.txt-mesa').style.display = 'none';
-        document.querySelector('.img-mesa').style.display = 'none';
-        document.querySelector('.barrita-mesa').style.display = 'none';
+        txtMesa.style.display = 'none';
+        imgMesa.style.display = 'none';
+        barritaMesa.classList.remove('completa');
+        btnMesa.classList.remove('completa');
+        
+        
+       
     });
 
-    btnMesa.addEventListener('click', function() {
+    btnMesa.addEventListener('click', function () {
         // Mostrar texto e imagen de la mesa
-        document.querySelector('.txt-mesa').style.display = 'flex';
-        document.querySelector('.img-mesa').style.display = 'block';
-        document.querySelector('.barrita-mesa').style.display = 'block';
-
+        txtMesa.style.display = 'flex';
+        imgMesa.style.display = 'block';
+        barritaMesa.style.display = 'block';
+        barritaMesa.classList.toggle('completa');
+        btnMesa.classList.toggle('completa');
+        
         // Ocultar texto e imagen de la silla
-        document.querySelector('.txt-silla').style.display = 'none';
-        document.querySelector('.img-silla').style.display = 'none';
-        document.querySelector('.barrita-silla').style.display = 'none';
-    });
-;
- // Función para manejar el scroll
- function handleScroll() {
-    // Obtener la posición del scroll vertical
-    const scrollPosition = window.scrollY;
-
-    // Altura del contenedor de texto
-    const textosHeight = document.querySelector('.txt-autor').offsetHeight;
-
-    // Determinar cuándo cambiar de elementos visibles
-    if (scrollPosition > textosHeight) {
-        // Mostrar img-mesa y txt-mesa, ocultar img-silla y txt-silla
+        txtSilla.style.display = 'none';
         imgSilla.style.display = 'none';
+        barritaSilla.classList.remove('completa');
+        btnSilla.classList.remove('completa');
+        
+        
+    });
+
+    ;
+    // Función para manejar el scroll
+    function hacerScroll() {
+        // Obtener la posición del scroll vertical
+        const posicionScroll = window.scrollY;
+
+        // Altura del contenedor de texto
+        const alturaTexto = document.querySelector('.txt-autor').offsetHeight;
+
+        // Determinar cuándo cambiar de elementos visibles
+        if (posicionScroll > alturaTexto) {
+            // Mostrar img-mesa y txt-mesa, ocultar img-silla y txt-silla
+            imgSilla.style.display = 'none';
             txtSilla.style.display = 'none';
             imgMesa.style.display = 'block';
             txtMesa.style.display = 'flex';
-    } else {
-        // Mostrar img-silla y txt-silla, ocultar img-mesa y txt-mesa
-        imgSilla.style.display = 'block';
+        } else {
+            // Mostrar img-silla y txt-silla, ocultar img-mesa y txt-mesa
+            imgSilla.style.display = 'block';
             txtSilla.style.display = 'flex';
             imgMesa.style.display = 'none';
             txtMesa.style.display = 'none';
+        }
     }
-}
 
-// Llamar a handleScroll cuando se haga scroll en la página
-window.addEventListener('scroll', handleScroll);
+    // Llamar a hacerScroll cuando se haga scroll en la página
+    window.addEventListener('scroll', hacerScroll);
 
-// Llamar a handleScroll al cargar la página para inicializar el estado
-handleScroll();
-;});
+    // Llamar a hacerScroll al cargar la página para inicializar el estado
+    hacerScroll();
+    ;
+});
+
+
+// document.querySelector('.footer-objeto_01').addEventListener('click', function () {
+
+//     const barraSilla = document.querySelector('.barrita-silla');
+//     barraSilla.classList.toggle('completa'); 
+    
+// });
+// document.querySelector('.footer-objeto_02').addEventListener('click', function () {
+
+//     const barraMesa = document.querySelector('.barrita-mesa');
+//     barraMesa.classList.toggle('completa'); 
+    
+// });
+
